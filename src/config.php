966c2eade805214b8ca454feee7b3cceafdb2c62
yunwuxin <448901948@qq.com>
2016-06-24 12:16:20 +0800

    注册指令

--- src/config.php
+++ src/config.php
@@ -12,5 +12,6 @@
 \think\Console::addDefaultCommands([
     "think\\queue\\command\\Work",
     "think\\queue\\command\\Restart",
-    "think\\queue\\command\\Listen"
+    "think\\queue\\command\\Listen",
+    "think\\queue\\command\\Subscribe"
 ]);
